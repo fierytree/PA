d1ceba3358fff9249e9db158cb85a95183103124
fierytree <1336521046@qq.com>
2021-03-15 18:54:05 +0800

    first commit

---
+++ nemu/src/monitor/debug/expr.c
@@ -0,0 +1,252 @@
+#include "nemu.h"
+
+/* We use the POSIX regex functions to process regular expressions.
+ * Type 'man regex' for more information about POSIX regex functions.
+ */
+#include <sys/types.h>
+#include <regex.h>
+
+enum {
+  TK_NOTYPE = 256, TK_EQ,
+  TK_NUMBER,
+  TK_HEX,
+  TK_REG,
+  TK_AND,
+  TK_OR,
+  TK_NEQ,
+  TK_NEGATIVE,
+
+  /* TODO: Add more token types */
+
+};
+
+static struct rule {
+  char *regex;
+  int token_type;
+} rules[] = {
+
+  /* TODO: Add more rules.
+   * Pay attention to the precedence level of different rules.
+   */
+
+  {" +", TK_NOTYPE},    // spaces
+  {"0|[1-9][0-9]*",TK_NUMBER},
+  {"0x[1-9A-Fa-f][0-9A-Fa-f]*",TK_HEX},
+  {"\\$(eax||ecx||edx||ebx||esp||ebp||esi||edi||eip
+  ||ax||cx||dx||bx||sp||bp||si||di||al||cl||dl||bl||ah||ch||dh||bh)",TK_REG}
+  {"!=",TK_NEQ},
+  {"&&",TK_AND},
+  {"\\|\\|",TK_OR},
+  {"!",'!'}
+
+  {"-",'-'},
+  {"\\+",'+'},
+  {"\\*",'*'},
+  {"\\/",'/'},
+  {"\\(",'('},
+  {"\\)", ')'},         // plus
+  {"==", TK_EQ}         // equal
+};
+
+#define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
+
+static regex_t re[NR_REGEX];
+
+/* Rules are used for many times.
+ * Therefore we compile them only once before any usage.
+ */
+void init_regex() {
+  int i;
+  char error_msg[128];
+  int ret;
+
+  for (i = 0; i < NR_REGEX; i ++) {
+    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+    if (ret != 0) {
+      regerror(ret, &re[i], error_msg, 128);
+      panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+    }
+  }
+}
+
+typedef struct token {
+  int type;
+  char str[32];
+} Token;
+
+Token tokens[32];
+int nr_token;
+
+static bool make_token(char *e) {
+  int position = 0;
+  int i;
+  regmatch_t pmatch;
+
+  nr_token = 0;
+
+  while (e[position] != '\0') {
+    /* Try all rules one by one. */
+    for (i = 0; i < NR_REGEX; i ++) {
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+        char *substr_start = e + position;
+        int substr_len = pmatch.rm_eo;
+
+        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        position += substr_len;
+
+        /* TODO: Now a new token is recognized with rules[i]. Add codes
+         * to record the token in the array `tokens'. For certain types
+         * of tokens, some extra actions should be performed.
+         */
+        if(substr_len>32)assert(0);
+        if(rules[i].token_type==TK_NOTYPE)break;
+        else{
+          tokens[nr_token].type=rules[i].token_type;
+          switch(rules[i].token_type){
+            case TK_NUMBER:{
+              strncpy(tokens[nr_token].str,substr_start,substr_len);
+              *(tokens[nr_token].str+substr_len)='\0';
+              break;
+            }
+            case TK_HEX:{
+              strncpy(tokens[nr_token].str,substr_start+2,substr_len);
+              *(tokens[nr_token].str+substr_len)='\0';
+              break;
+            }
+            case TK_REG:{
+              strncpy(tokens[nr_token].str,substr_start+1,substr_len);
+              *(tokens[nr_token].str+substr_len)='\0';
+              break;
+            }
+            //default:;
+          }
+          //printf("%d",nr_token);
+          nr_token+=1;
+        }
+        break;
+      }
+    }
+
+    if (i == NR_REGEX) {
+      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+      return false;
+    }
+  }
+
+  return true;
+}
+bool check_parentheses(int p,int q){
+  if(p>=q){
+    printf("error:p>=q in check_parentheses\n");
+    return false;
+  }
+  if(tokens[p].type!='('||tokens[q].type!=')')
+    return false;
+  int count=0;
+  for(int curr=p+1;curr<q;curr++){
+    if(tokens[curr].type=='(')count+=1;
+    if(tokens[curr].type==')'){
+      if(count!=0)count-=0;
+      else return false;
+    }
+  }
+  if(count==0)return true;
+  else return false;
+}
+int findDominantOp(int p,int q){
+  int br_level=0;
+  int min_op=1e9+1;
+  int pos=-1;
+  int op=0;
+  for(int curr=p+1;curr<q;curr++){
+    if(tokens[curr].type=='(')br_level+=1;
+    if(tokens[curr].type==')')br_level-=1;
+    if(br_level==0){
+      if(tokens[curr].type==TK_NUMBER)continue;
+      else if(tokens[curr].type=='-'&&(curr==p||tokens[curr-1].type!=TK_NUMBER))tokens[curr].type=TK_NEGATIVE,op=13;
+      else if(tokens[curr].type=='+'||tokens[curr].type=='-')op=10;
+      else if(tokens[curr].type=='*'||tokens[curr].type=='/')op=11;
+      else if(tokens[curr].type=='||'||tokens[curr].type=='&&')op=8;
+      else if(tokens[curr].type=='!')op=12;
+      else if(tokens[curr].type=='=='||tokens[curr].type=='!=')op=9;
+
+      if(op<min_op)min_op=op,pos=curr;
+      else if(op==min_op)pos=curr;
+    }
+  }
+  return pos;
+}
+int eval(int p,int q){
+  if(p>q){
+    printf("error:p>q in eval %d %d\n",p,q);
+   assert(0);
+  }
+  else if(p==q){
+    if(tokens[p].type!=TK_NUMBER){
+      printf("error: single op");
+      assert(0);
+    }
+    else{
+      int num;
+      switch(tokens[p].type){
+        case TK_NUMBER:{
+          sscanf(tokens[p].str,"%d",&num);break;
+        }
+        case TK_HEX:{
+          sscanf(tokens[p].str,"%x",&num)break;
+        }
+        case TK_REG:{
+          for(int i=0;i<8;i++){
+            if(strcmp(tokens[p].str,regsl[i])==0)
+            return reg_l(i);
+            if(strcmp(tokens[p].str,regsw[i])==0)
+            return reg_w(i);
+            if(strcmp(tokens[p].str,regsb[i])==0)
+            return reg_b(i);
+          }
+        }
+      }
+      return num;
+    }
+  }
+  else if(check_parentheses(p,q)==true){
+    return eval(p+1,q-1);
+  }
+  else{
+    int pos=findDominantOp(p,q);
+    vaddr_t addr;int result;
+    switch(tokens[pos].type){
+      case TK_NEGATIVE:return -eval(p+1,q);
+      case '!':{
+        result=eval(p+1,q);
+        if(result==0)return 1;
+        else return 0;
+      }
+
+    }
+    int val1=eval(p,pos-1);
+    int val2=eval(pos+1,q);
+    switch(tokens[pos].type){
+      case '+': return val1+val2;
+      case '-': return val1-val2;
+      case '*': return val1*val2;
+      case '/': return val1/val2;
+      case TK_EQ:return val1==val2;
+      case TK_NEQ:return val1!=val2;
+      case TK_AND:return val1&&val2;
+      case TK_OR:return val1||val2;
+      default: assert(0);
+    }
+  }
+}
+uint32_t expr(char *e, bool *success) {
+  if (!make_token(e)) {
+    *success = false;
+    return 0;
+  }
+
+  /* TODO: Insert codes to evaluate the expression. */
+  *success=true;
+  return eval(0,nr_token-1);
+}
---
+++ nemu/src/monitor/debug/ui.c
@@ -0,0 +1,188 @@
+#include "monitor/monitor.h"
+#include "monitor/expr.h"
+#include "monitor/watchpoint.h"
+#include "nemu.h"
+
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+void cpu_exec(uint64_t);
+
+/* We use the `readline' library to provide more flexibility to read from stdin. */
+char* rl_gets() {
+  static char *line_read = NULL;
+
+  if (line_read) {
+    free(line_read);
+    line_read = NULL;
+  }
+
+  line_read = readline("(nemu) ");
+
+  if (line_read && *line_read) {
+    add_history(line_read);
+  }
+
+  return line_read;
+}
+
+static int cmd_c(char *args) {
+  cpu_exec(-1);
+  return 0;
+}
+
+static int cmd_q(char *args) {
+  return -1;
+}
+
+static int cmd_help(char *args);
+static int cmd_si(char *args);
+static int cmd_info(char *args);
+static int cmd_x(char *args);
+static int cmd_p(char *args);
+static int cmd_w(char *args);
+static int cmd_d(char *args);
+
+static struct {
+  char *name;
+  char *description;
+  int (*handler) (char *);
+} cmd_table [] = {
+  { "help", "Display informations about all supported commands", cmd_help },
+  { "c", "Continue the execution of the program", cmd_c },
+  { "q", "Exit NEMU", cmd_q },
+  {"si","args:[N];execute [N] instructions step by step",cmd_si},
+  {"info","args:r/w;print information about registers or watchpoint",cmd_info},
+  {"x","x [N] [EXPR];scan the memory",cmd_x},
+  {"p","expr",cmd_p},
+  {"w","set the watchpoint",cmd_w},
+  {"d","delete the watch point",cmd_d},
+  /* TODO: Add more commands */
+
+};
+
+#define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+
+static int cmd_help(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  int i;
+
+  if (arg == NULL) {
+    /* no argument given */
+    for (i = 0; i < NR_CMD; i ++) {
+      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    }
+  }
+  else {
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        return 0;
+      }
+    }
+    printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
+
+void ui_mainloop(int is_batch_mode) {
+  if (is_batch_mode) {
+    cmd_c(NULL);
+    return;
+  }
+
+  while (1) {
+    char *str = rl_gets();
+    char *str_end = str + strlen(str);
+
+    /* extract the first token as the command */
+    char *cmd = strtok(str, " ");
+    if (cmd == NULL) { continue; }
+
+    /* treat the remaining string as the arguments,
+     * which may need further parsing
+     */
+    char *args = cmd + strlen(cmd) + 1;
+    if (args >= str_end) {
+      args = NULL;
+    }
+
+#ifdef HAS_IOE
+    extern void sdl_clear_event_queue(void);
+    sdl_clear_event_queue();
+#endif
+
+    int i;
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(cmd, cmd_table[i].name) == 0) {
+        if (cmd_table[i].handler(args) < 0) { return; }
+        break;
+      }
+    }
+
+    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+  }
+}
+
+static int cmd_si(char *args){
+  uint64_t N=0;
+  if(args==NULL)N=1;
+  else{
+    int nRet=sscanf(args,"%llu",&N);
+    if(nRet<=0){
+      printf("args error in cmd_si\n");
+      return 0;
+    }
+  }
+  cpu_exec(N);
+  return 0;
+}
+static int cmd_info(char *args){
+  char s;
+  if(args==NULL){
+    printf("args error in cmd_info\n");
+    return 0;
+  }
+  int nRet=sscanf(args,"%c",&s);
+  if(nRet<=0){
+    printf("args error in cmd_info\n");
+    return 0;
+  }
+  if(s=='r'){
+    int i;
+    for(i=0;i<8;i++)printf("%s\t0x%x\n",regsl[i],reg_l(i));
+    printf("eip\t0x%x\n",cpu.eip);
+    for(i=0;i<8;i++)printf("%s\t0x%x\n",regsw[i],reg_w(i));
+    for(i=0;i<8;i++)printf("%s\t0x%x\n",regsb[i],reg_b(i));
+    return 0;
+  }
+  printf("args error in cmd_info\n");
+  return 0;
+}
+static int cmd_x(char *args){
+  int nlen=0;
+  vaddr_t addr;
+  int nRet=sscanf(args,"%d 0x%x",&nlen,&addr);
+  if(nRet<=0){
+    printf("args error in cmd_x\n");
+    return 0;
+  }
+  printf("memory:");
+  for(int i=0;i<nlen;i++){
+    if(i%4==0)printf("\n0x%x:\t0x%02x",addr+i,vaddr_read(addr+i,1));
+    else printf("\t0x%02x",vaddr_read(addr+i,1));
+  }
+  printf("\n");
+  return 0;
+}
+static int cmd_p(char *args){
+  bool success;
+  int res=expr(args,&success);
+  if(success==false)printf("error in expr()\n");
+  else printf("the value of expr is:%d\n", res);
+  return 0;
+}
+static int cmd_w(char *args){return 0;}
+static int cmd_d(char *args){return 0;}
