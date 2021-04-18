feb88cb6cdb20e5e4d221d1324bba783023e56b2
tracer-ics2017 <tracer@njuics.org>
2021-04-18 09:41:51 -0700

    >  compile 1812979 fierytrees Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:41:51 up 9 min,  1 user,  load average: 0.35, 0.19, 0.11 e3539cdff8ca56c85004315dfc0a800e6c12fcc3

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -1,14 +1,25 @@
 #include "cpu/exec.h"
+static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1);
+static inline void rtl_not(rtlreg_t* dest);
+static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t *src1,int width);
 
 make_EHelper(test) {
-  TODO();
-
+  //TODO();
+  rtl_and(&t2,&id_dest->val,&id_src->val);
+  rtl_update_ZFSF(&t2,id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
   print_asm_template2(test);
 }
 
 make_EHelper(and) {
-  TODO();
+  //TODO();
+  rtl_and(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
 
+  rtl_update_ZFSF(&t2,id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
   print_asm_template2(and);
 }
 
@@ -24,28 +35,44 @@ make_EHelper(xor) {
 }
 
 make_EHelper(or) {
-  TODO();
+  //TODO();
+  rtl_or(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+  rtl_update_ZFSF(&t2,id_dest->width);
+  rtl_set_CF(&tzero);
+  rtl_set_OF(&tzero);
 
   print_asm_template2(or);
 }
 
 make_EHelper(sar) {
-  TODO();
+  //TODO();
   // unnecessary to update CF and OF in NEMU
+  rtl_sext(&t2,&id_dest->val,id_dest->width);
+  rtl_sar(&t2,&t2,&id_src->val);
+  operand_write(id_dest,&t2);
+  rtl_update_ZFSF(&t2,id_dest->width);
 
   print_asm_template2(sar);
 }
 
 make_EHelper(shl) {
-  TODO();
+  //TODO();
   // unnecessary to update CF and OF in NEMU
+
+  rtl_shl(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+  rtl_update_ZFSF(&t2,id_dest->width);
 
   print_asm_template2(shl);
 }
 
 make_EHelper(shr) {
-  TODO();
+  //TODO();
   // unnecessary to update CF and OF in NEMU
+  rtl_shr(&t2,&id_dest->val,&id_src->val);
+  operand_write(id_dest,&t2);
+  rtl_update_ZFSF(&t2,id_dest->width);
 
   print_asm_template2(shr);
 }
@@ -59,7 +86,9 @@ make_EHelper(setcc) {
 }
 
 make_EHelper(not) {
-  TODO();
+  //TODO();
+  rtl_not(&id_dest->val);
+  operand_write(id_dest,&id_dest->val);
 
   print_asm_template1(not);
 }
