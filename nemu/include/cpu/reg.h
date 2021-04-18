224ee5da2b01ffdad55ca5cdbc23a84958961715
tracer-ics2017 <tracer@njuics.org>
2021-04-18 05:53:36 -0700

    >  compile 1812979 fierytrees Linux ubuntu 4.15.0-140-generic #144~16.04.1-Ubuntu SMP Fri Mar 19 21:23:00 UTC 2021 i686 i686 i686 GNU/Linux  05:53:36 up 12 min,  1 user,  load average: 0.12, 0.24, 0.25 381cf170cca283cdbcd57a47ee4a4582f784653d

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -30,6 +30,19 @@ typedef struct {
    * in PA2 able to directly access these registers.
    */
   vaddr_t eip;
+  struct bs{
+    unsigned int CF:1;
+    unsigned int one:1;
+    unsigned int :4;
+    unsigned int ZF:1;
+    unsigned int SF:1;
+
+    unsigned int :1;
+    unsigned int IF:1;
+    unsigned int :1;
+    unsigned int OF:1;
+    unsigned int :20;
+  }eflags;
 } CPU_state;
 
 extern CPU_state cpu;
