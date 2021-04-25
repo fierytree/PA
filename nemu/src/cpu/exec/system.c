a4b6aeacb51279892475370914eb7a3bf4a168b5
tracer-ics2017 <tracer@njuics.org>
2021-04-25 07:23:47 -0700

    >  compile 1812979 fierytrees Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  07:23:47 up  5:45,  1 user,  load average: 0.01, 0.04, 0.08 96ac4f65b4a36b3a3c18c8cb4d63992c1c5f6a8b

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -58,7 +58,7 @@ make_EHelper(in) {
 
 make_EHelper(out) {
   //TODO();
-  pio_write(id_dest->val,id_src->val,id_src->width);
+  pio_write(id_dest->val,id_src->width,id_src->val);
   print_asm_template2(out);
 
 #ifdef DIFF_TEST
