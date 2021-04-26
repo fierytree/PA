1b7dba5bca7ae81e54e57495a04fc99e1a7d71dc
tracer-ics2017 <tracer@njuics.org>
2021-04-26 00:31:44 -0700

    >  compile 1812979 fierytrees Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  00:31:44 up 1 min,  1 user,  load average: 2.06, 0.79, 0.29 1f16acc3fd01137648161e9715e6ce0bb3d2efed

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -107,7 +107,7 @@ make_EHelper(rol) {
   assert(id_src->val==(cpu.ecx&0xff));
   assert(id_dest->val==cpu.eax);
   assert(id_src->val<32);
-  cpu.eax=(cpu.eax<<id_src->val)|(cpu.eax>>id_src->val);
+  cpu.eax=(cpu.eax<<id_src->val)|(32-(cpu.eax>>id_src->val));
 
   print_asm_template2(rol);
 }
