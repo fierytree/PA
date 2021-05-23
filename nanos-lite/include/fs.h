3c7333058a3ade23895015edba3d8643174d3c31
tracer-ics2017 <tracer@njuics.org>
2021-05-23 09:32:15 -0700

    >  run 1812979 fierytrees Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  09:32:15 up  8:08,  1 user,  load average: 0.23, 0.35, 0.35 64046c5ec0a14199e0da213dd504985edd224a12

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -12,4 +12,4 @@ int fs_open(const char* filename, int flags,int mode);
 ssize_t fs_read(int fd,void* buf,size_t len);
 ssize_t fs_write(int fd,void* buf,size_t len);
 int fs_close(int fd);
-off_t fs_lsseek(int fd,off_t offset,int whence);
+off_t fs_lseek(int fd,off_t offset,int whence);
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "syscall.h"
+#include "fs.h"
 
 int sys_none(){
   return 1;
@@ -13,19 +14,36 @@ int sys_write(int fd,void* buf,size_t len){
   // panic("fd=%d in sys_write",fd);
   if(fd==1||fd==2){
     char c;
-    Log("buffer:%s",(char*)buf);
+    // Log("buffer:%s",(char*)buf);
     for(int i=0;i<len;i++){
       memcpy(&c,buf+i,1);
       _putc(c);
     }
     return len;
   }
-  else panic("Unhandled fd=%d in sys_write",fd);
+  if(fd>=3)return fs_write(fd,buf,len);
+  Log("fd=%d",fd);
   return -1;
 }
 
 int sys_brk(int ddr){
   return 0;
+}
+
+int sys_open(const char*filename){
+  return fs_open(filename,0,0);
+}
+
+int sys_read(int fd,void* buf,size_t len){
+  return fs_read(fd,buf,len);
+}
+
+int sys_close(int fd){
+  return fs_close(fd);
+}
+
+int sys_lseek(int fd,off_t offset,int whence){
+  return fs_lseek(fd,offset,whence);
 }
 
 _RegSet* do_syscall(_RegSet *r) {
@@ -42,6 +60,10 @@ _RegSet* do_syscall(_RegSet *r) {
     case SYS_exit:sys_exit(a[1]);break;
     case SYS_write:SYSCALL_ARG1(r)=sys_write(a[1],(void*)a[2],a[3]);break;
     case SYS_brk:SYSCALL_ARG1(r)=sys_brk(a[1]);break;
+    case SYS_open:SYSCALL_ARG1(r)=sys_open((char*)a[1]);break;
+    case SYS_read:SYSCALL_ARG1(r)=sys_read(a[1],(void*)a[2],a[3]);break;
+    case SYS_close:SYSCALL_ARG1(r)=sys_close(a[1]);break;
+    case SYS_lseek:SYSCALL_ARG1(r)=sys_lseek(a[1],a[2],a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
