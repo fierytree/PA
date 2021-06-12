75077a67363b8241dd65de966c631947b4835eb6
tracer-ics2017 <tracer@njuics.org>
2021-06-12 10:38:57 -0700

    >  run 1812979 fierytrees Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:38:57 up  2:43,  1 user,  load average: 0.72, 0.43, 0.42 910678e2d9697d0dfeb2e8ed5f14716190759026

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -36,7 +36,7 @@ int main() {
   //uint32_t entry=loader(NULL, "/bin/hello");
   //((void (*)(void))entry)();
   extern void load_prog(const char *filename);
-  load_prog("/bin/bmptest");
+  load_prog("/bin/pal");
 
   panic("Should not reach here");
 }
