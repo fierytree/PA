ef18697ae9a0c1c05c3758d2212f8acbd12a7115
tracer-ics2017 <tracer@njuics.org>
2021-06-16 04:58:31 -0700

    >  run 1812979 fierytrees Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  04:58:31 up  1:04,  1 user,  load average: 0.74, 1.29, 1.20 ba4acef12f9dd1a51578e7694dfce5828c25f431

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -11,7 +11,7 @@ static _RegSet* do_event(_Event e, _RegSet* r) {
       printf("event:self-trapped\n");
       return schedule(r);
     case _EVENT_IRQ_TIME:
-      Log("event:URQ_TIME");
+      //Log("event:URQ_TIME");
       return schedule(r);
     default: panic("Unhandled event ID = %d", e.event);
   }
