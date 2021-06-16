54e9c05ddff666e4cb7309799090027f568b46f7
tracer-ics2017 <tracer@njuics.org>
2021-06-16 05:14:56 -0700

    >  run 1812979 fierytrees Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  05:14:56 up  1:20,  1 user,  load average: 1.95, 1.68, 1.36 888570f69478b4be7988a53df39ff5be5ce0970a

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -16,6 +16,11 @@ size_t events_read(void *buf, size_t len) {
     key^=0x8000;
     down=true;
   }
+  if(down&&key==_KEY_F12){
+    extern void switch_current_game();
+    switch_current_game();
+    Log("switch");
+  }
   if(key!=_KEY_NONE)
   sprintf(str,"%s %s\n",down?"kd":"ku",keyname[key]);
   else sprintf(str,"t %d\n",_uptime());
--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -38,6 +38,7 @@ int main() {
   extern void load_prog(const char *filename);
   load_prog("/bin/pal");
   load_prog("/bin/hello");
+  load_prog("/bin/videotest");
 
   _trap();
   panic("Should not reach here");
